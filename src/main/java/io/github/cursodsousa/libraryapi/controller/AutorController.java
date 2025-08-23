9b9cd902c80fb2ee908b36e78b98956afa5a7a41
tiago <tiago@MacBookPro.lan>
2025-08-22 22:56:44 -1000

    add GET filter users by name and nacionalidade, and PUT

--- src/main/java/io/github/cursodsousa/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/AutorController.java
@@ -3,14 +3,15 @@ package io.github.cursodsousa.libraryapi.controller;
 import io.github.cursodsousa.libraryapi.controller.dto.AutorDTO;
 import io.github.cursodsousa.libraryapi.model.Autor;
 import io.github.cursodsousa.libraryapi.service.AutorService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -52,5 +53,53 @@ public class AutorController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") String id){
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+
+        if(autorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        service.deletar(autorOptional.get());
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value="nome", required=false) String nome,
+            @RequestParam(value="nacionalidade", required=false) String nacionalidade
+    ){
+        List<Autor> resultado = service.pesquisa(nome, nacionalidade);
+        List<AutorDTO> lista = resultado
+                                .stream()
+                                .map(autor -> new AutorDTO(autor.getId(),
+                                                                autor.getNome(),
+                                                                autor.getDataNascimento(),
+                                                                autor.getNacionalidade())
+                                ).collect(Collectors.toList());
+        return ResponseEntity.ok(lista);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable("id") String id, @RequestBody AutorDTO dto){
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+
+        if(autorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var autor = autorOptional.get();
+        autor.setNome(dto.nome());
+        autor.setNacionalidade(dto.nacionalidade());
+        autor.setDataNascimento(dto.dataNascimento());
+
+        service.atualizar(autor);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/io/github/cursodsousa/libraryapi/service/AutorService.java
+++ src/main/java/io/github/cursodsousa/libraryapi/service/AutorService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
 
 @Service
 public class AutorService {
@@ -16,11 +17,40 @@ public class AutorService {
         this.repository = repository;
     }
 
+    //service para salvar autor
     public Autor salvar(Autor autor){
         return repository.save(autor);
     }
 
+    public void atualizar(Autor autor){
+        if(autor.getId() == null) {
+            throw new IllegalArgumentException("Para atualizar é necessário que o autor já esteja salvo na base.");
+        }
+        repository.save(autor);
+    }
+
+    //service para buscar autor por id
     public Optional<Autor> obterPorId(UUID id){
         return repository.findById(id);
+    }
+
+    public void deletar(Autor autor){
+        repository.delete(autor);
+    }
+    //aqui fica a logica
+    public List<Autor> pesquisa(String nome, String nacionalidade){
+        if(nome != null && nacionalidade != null){
+            return repository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+
+        if(nome !=  null){
+            return repository.findByNome(nome);
+        }
+
+        if (nacionalidade != null){
+            return repository.findByNacionalidade(nacionalidade);
+        }
+
+        return repository.findAll();
     }
 }
