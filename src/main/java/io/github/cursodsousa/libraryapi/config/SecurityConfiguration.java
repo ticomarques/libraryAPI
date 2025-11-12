3699ceb9a25ac064701a345f6f92f2ec315017d5
tiago <tiago@tiagos-MacBook-Pro.local>
2025-11-12 05:46:51 -0300

    add logginh and observer actuator

--- src/main/java/io/github/cursodsousa/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/cursodsousa/libraryapi/config/SecurityConfiguration.java
@@ -56,7 +56,8 @@ public class SecurityConfiguration {
                     "/swagger-resources/**",
                     "/swagger-ui.html",
                     "/swagger-ui/**",
-                    "/webjars/**"
+                    "/webjars/**",
+                    "/actuator/**"
             );
     }
 
